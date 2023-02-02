a998dd226a0945a2da744c714b45dfa92b216a5f
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-02-02 18:43:08 +0300

    make AssimpAssetSerializatorModule auto add module

--- MoonEngine-Root/Assimp/src/main/java/module-info.java
+++ MoonEngine-Root/Assimp/src/main/java/module-info.java
@@ -1,3 +1,6 @@
+import com.greentree.engine.moon.mesh.assimp.assets.AssimpAssetSerializatorModule;
+import com.greentree.engine.moon.module.EngineModule;
+
 open module engine.moon.assimp {
 	
 	requires transitive engine.moon.base;
@@ -8,5 +11,7 @@ open module engine.moon.assimp {
 	requires org.lwjgl.assimp;
 	
 	exports com.greentree.engine.moon.mesh.assimp.assets;
+	
+	provides EngineModule with AssimpAssetSerializatorModule;
 	
 }
