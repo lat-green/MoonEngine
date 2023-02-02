03f13e27445ea98c40089f838a70a73400ef20d3
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-02-02 18:51:38 +0300

    Update FileWatcherPollEventsModule.java

--- MoonEngine-Root/Base/src/main/java/com/greentree/engine/moon/base/FileWatcherPollEventsModule.java
+++ MoonEngine-Root/Base/src/main/java/com/greentree/engine/moon/base/FileWatcherPollEventsModule.java
@@ -3,7 +3,7 @@ package com.greentree.engine.moon.base;
 import com.greentree.commons.data.FileWatcher;
 import com.greentree.engine.moon.module.UpdateModule;
 
-public class FileWatcherPollEventsSystem implements UpdateModule {
+public class FileWatcherPollEventsModule implements UpdateModule {
 	
 	@Override
 	public void update() {
