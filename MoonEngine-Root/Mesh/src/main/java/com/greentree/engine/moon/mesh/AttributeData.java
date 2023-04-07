f179c738b1f376edc28e5e8ddd2eb7f1ce6cdb77
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-04-07 10:07:16 +0300

    ceche Meshes

--- MoonEngine-Root/Mesh/src/main/java/com/greentree/engine/moon/mesh/AttributeData.java
+++ MoonEngine-Root/Mesh/src/main/java/com/greentree/engine/moon/mesh/AttributeData.java
@@ -12,6 +12,27 @@ public record AttributeData(float[] vertex, int... sizes) implements Serializabl
 	}
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(sizes);
+		result = prime * result + Arrays.hashCode(vertex);
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		if(!(obj instanceof AttributeData)) {
+			return false;
+		}
+		AttributeData other = (AttributeData) obj;
+		return Arrays.equals(sizes, other.sizes) && Arrays.equals(vertex, other.vertex);
+	}
+	
+	@Override
 	public String toString() {
 		return "AttributeData [" + Arrays.toString(vertex) + ", " + Arrays.toString(sizes) + "]";
 	}
