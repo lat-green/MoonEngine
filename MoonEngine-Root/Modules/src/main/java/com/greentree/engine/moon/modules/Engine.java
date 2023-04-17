867192f7377724c13a124fc71adca7e85e3075e8
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-04-17 18:08:44 +0300

    Update Engine.java

--- MoonEngine-Root/Modules/src/main/java/com/greentree/engine/moon/modules/Engine.java
+++ MoonEngine-Root/Modules/src/main/java/com/greentree/engine/moon/modules/Engine.java
@@ -12,19 +12,9 @@ public final class Engine {
 	private Engine() {
 	}
 	
-	private static Class<?> getCaller() {
-		try {
-			return Class.forName(Thread.currentThread().getStackTrace()[3].getClassName());
-		}catch(ClassNotFoundException e) {
-			throw new RuntimeException("unreal exception", e);
-		}
-	}
-	
 	public static void launch(String[] args, EngineModule... modules) {
 		System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
 		System.setErr(new PrintStream(System.err, true, StandardCharsets.UTF_8));
-		
-		var cls = getCaller();
 		
 		var allModules = new ArrayList<EngineModule>();
 		
