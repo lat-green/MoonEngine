13bd2f53cdd07a89ad69c63dc670c532246885da
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-05-31 21:30:25 +0300

    Chenge logging

--- MoonEngine-Root/Modules/src/main/java/com/greentree/engine/moon/modules/Engine.java
+++ MoonEngine-Root/Modules/src/main/java/com/greentree/engine/moon/modules/Engine.java
@@ -1,47 +1,49 @@
 package com.greentree.engine.moon.modules;
 
+import com.greentree.engine.moon.modules.property.EnginePropertiesBase;
+import com.greentree.engine.moon.modules.property.ExitManagerProperty;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.greentree.engine.moon.modules.property.EnginePropertiesBase;
-import com.greentree.engine.moon.modules.property.ExitManagerProperty;
-
 public final class Engine {
-	
-	private static Logger logger = LogManager.getLogger(Engine.class);
-	
-	private Engine() {
-	}
-	
-	public static void launch(String[] args, LaunchModule launchModule, UpdateModule updateModule,
-			TerminateModule terminateModule) {
-		var exitManager = new ExitManagerImpl();
-		var properties = new EnginePropertiesBase();
-		properties.add(new ExitManagerProperty(exitManager));
-		logger.info(() -> "Engine.launch");
-		launchModule.launch(properties);
-		while(!exitManager.isShouldExit()) {
-			logger.info(() -> "Engine.update");
-			updateModule.update();
-		}
-		logger.info(() -> "Engine.terminate");
-		terminateModule.terminate();
-	}
-	
+
+    private static final Logger logger = LogManager.getLogger(Engine.class);
+
+    private Engine() {
+    }
+
+    public static void launch(String[] args, LaunchModule launchModule, UpdateModule updateModule,
+                              TerminateModule terminateModule) {
+        var exitManager = new ExitManagerImpl();
+        var properties = new EnginePropertiesBase();
+        properties.add(new ExitManagerProperty(exitManager));
+        logger.info(() -> "Begin Engine.launch");
+        launchModule.launch(properties);
+        logger.info(() -> "End Engine.launch");
+        while (!exitManager.isShouldExit()) {
+            logger.info(() -> "Begin Engine.update");
+            updateModule.update();
+            logger.info(() -> "End Engine.update");
+        }
+        logger.info(() -> "Begin Engine.terminate");
+        terminateModule.terminate();
+        logger.info(() -> "End Engine.terminate");
+    }
+
 }
 
 class ExitManagerImpl implements ExitManager {
-	
-	private boolean shouldExit = false;
-	
-	@Override
-	public void exit() {
-		shouldExit = true;
-	}
-	
-	@Override
-	public boolean isShouldExit() {
-		return shouldExit;
-	}
-	
+
+    private boolean shouldExit = false;
+
+    @Override
+    public void exit() {
+        shouldExit = true;
+    }
+
+    @Override
+    public boolean isShouldExit() {
+        return shouldExit;
+    }
+
 }
