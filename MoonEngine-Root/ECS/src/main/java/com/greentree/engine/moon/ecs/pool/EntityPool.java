f55c366c6a3190b58985e634a275558a847cfa5a
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-06-04 01:53:18 +0300

    Update EntityPool.java

--- MoonEngine-Root/ECS/src/main/java/com/greentree/engine/moon/ecs/pool/EntityPool.java
+++ MoonEngine-Root/ECS/src/main/java/com/greentree/engine/moon/ecs/pool/EntityPool.java
@@ -2,8 +2,12 @@ package com.greentree.engine.moon.ecs.pool;
 
 import com.greentree.engine.moon.ecs.WorldEntity;
 
-public interface EntityPool {
+public interface EntityPool extends AutoCloseable {
 
     WorldEntity get();
+
+    @Override
+    default void close() {
+    }
 
 }
