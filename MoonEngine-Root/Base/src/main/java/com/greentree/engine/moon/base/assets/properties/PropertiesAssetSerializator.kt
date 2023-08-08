d50f0110c5457be371e265f73514f0859e23ea8e
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-08-09 00:49:23 +0300

    Update PropertiesAssetSerializator.kt

--- MoonEngine-Root/Base/src/main/java/com/greentree/engine/moon/base/assets/properties/PropertiesAssetSerializator.kt
+++ MoonEngine-Root/Base/src/main/java/com/greentree/engine/moon/base/assets/properties/PropertiesAssetSerializator.kt
@@ -1,37 +1,27 @@
 package com.greentree.engine.moon.base.assets.properties
 
 import com.greentree.commons.data.resource.Resource
-import com.greentree.commons.util.exception.WrappedException
 import com.greentree.engine.moon.assets.asset.Asset
 import com.greentree.engine.moon.assets.asset.Value1Function
-import com.greentree.engine.moon.assets.asset.isValid
 import com.greentree.engine.moon.assets.asset.map
 import com.greentree.engine.moon.assets.key.AssetKey
 import com.greentree.engine.moon.assets.serializator.AssetSerializator
 import com.greentree.engine.moon.assets.serializator.manager.AssetManager
-import java.io.IOException
+import com.greentree.engine.moon.assets.serializator.manager.load
 import java.util.*
 
 class PropertiesAssetSerializator : AssetSerializator<Properties> {
 
-	override fun load(context: AssetManager, ckey: AssetKey): Asset<Properties>? {
-		run {
-			val res = context.load(Resource::class.java, ckey)
-			if(res.isValid)
-				return res.map(ResourceToProperties)
-		}
-		return null
+	override fun load(context: AssetManager, ckey: AssetKey): Asset<Properties> {
+		val res = context.load<Resource>(ckey)
+		return res.map(ResourceToProperties)
 	}
 
 	private object ResourceToProperties : Value1Function<Resource, Properties> {
 
 		override fun apply(res: Resource): Properties {
 			val prop = Properties()
-			try {
-				res.open().use { `in` -> prop.load(`in`) }
-			} catch(e: IOException) {
-				throw WrappedException(e)
-			}
+			res.open().use { prop.load(it) }
 			return prop
 		}
 
