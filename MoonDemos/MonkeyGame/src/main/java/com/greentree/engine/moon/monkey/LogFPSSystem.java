8efebfc64f5150d7829bba8acc37a73182f9d17c
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-09-17 13:59:57 +0300

    Add LogFPSSystem to MonkeyGame

---
+++ MoonDemos/MonkeyGame/src/main/java/com/greentree/engine/moon/monkey/LogFPSSystem.java
@@ -0,0 +1,38 @@
+package com.greentree.engine.moon.monkey;
+
+import com.greentree.engine.moon.base.property.modules.ReadProperty;
+import com.greentree.engine.moon.base.time.Time;
+import com.greentree.engine.moon.ecs.scene.SceneProperties;
+import com.greentree.engine.moon.ecs.system.InitSystem;
+import com.greentree.engine.moon.ecs.system.UpdateSystem;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogFPSSystem implements InitSystem, UpdateSystem {
+
+    private static final Logger LOG = LogManager.getLogger(LogFPSSystem.class);
+
+    private static final float UPDATE_TIME = .1f;
+    private Time time;
+    private float t;
+    private int fps;
+
+    @ReadProperty({Time.class})
+    @Override
+    public void update() {
+        t += time.delta();
+        fps++;
+        while (t > UPDATE_TIME) {
+            t -= UPDATE_TIME;
+            LOG.info("fps: " + fps / UPDATE_TIME + " ms: " + 1f / (fps / UPDATE_TIME));
+            fps = 0;
+        }
+    }
+
+    @ReadProperty({Time.class})
+    @Override
+    public void init(SceneProperties properties) {
+        time = properties.get(Time.class);
+    }
+
+}
