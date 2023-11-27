fe4f1c80e0816fdd1b07426b61f83b113d725f78
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-11-27 16:34:00 +0300

    Update Groups.kt

--- MoonEngine/Assets/src/main/java/com/greentree/engine/moon/assets/asset/Groups.kt
+++ MoonEngine/Assets/src/main/java/com/greentree/engine/moon/assets/asset/Groups.kt
@@ -1,12 +1,12 @@
 package com.greentree.engine.moon.assets.asset
 
-fun <T1 : Any, T2 : Any> group(t1: T1, t2: T2) = Group2(t1, t2)
-fun <T1 : Any, T2 : Any, T3 : Any> group(t1: T1, t2: T2, t3: T3) = Group3(t1, t2, t3)
-fun <T1 : Any, T2 : Any, T3 : Any, T4 : Any> group(t1: T1, t2: T2, t3: T3, t4: T4) = Group4(t1, t2, t3, t4)
-fun <T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any> group(t1: T1, t2: T2, t3: T3, t4: T4, t5: T5) =
+fun <T1, T2> group(t1: T1, t2: T2) = Group2(t1, t2)
+fun <T1, T2, T3> group(t1: T1, t2: T2, t3: T3) = Group3(t1, t2, t3)
+fun <T1, T2, T3, T4> group(t1: T1, t2: T2, t3: T3, t4: T4) = Group4(t1, t2, t3, t4)
+fun <T1, T2, T3, T4, T5> group(t1: T1, t2: T2, t3: T3, t4: T4, t5: T5) =
 	Group5(t1, t2, t3, t4, t5)
 
-fun <T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any> group(
+fun <T1, T2, T3, T4, T5, T6> group(
 	t1: T1,
 	t2: T2,
 	t3: T3,
@@ -16,18 +16,18 @@ fun <T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any> group(
 ) =
 	Group6(t1, t2, t3, t4, t5, t6)
 
-data class Group2<T1 : Any, T2 : Any>(@JvmField val t1: T1, @JvmField val t2: T2)
+data class Group2<T1, T2>(@JvmField val t1: T1, @JvmField val t2: T2)
 
-data class Group3<T1 : Any, T2 : Any, T3 : Any>(@JvmField val t1: T1, @JvmField val t2: T2, @JvmField val t3: T3)
+data class Group3<T1, T2, T3>(@JvmField val t1: T1, @JvmField val t2: T2, @JvmField val t3: T3)
 
-data class Group4<T1 : Any, T2 : Any, T3 : Any, T4 : Any>(
+data class Group4<T1, T2, T3, T4>(
 	@JvmField val t1: T1,
 	@JvmField val t2: T2,
 	@JvmField val t3: T3,
 	@JvmField val t4: T4,
 )
 
-data class Group5<T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any>(
+data class Group5<T1, T2, T3, T4, T5>(
 	@JvmField val t1: T1,
 	@JvmField val t2: T2,
 	@JvmField val t3: T3,
@@ -35,7 +35,7 @@ data class Group5<T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any>(
 	@JvmField val t5: T5,
 )
 
-data class Group6<T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any>(
+data class Group6<T1, T2, T3, T4, T5, T6>(
 	@JvmField val t1: T1,
 	@JvmField val t2: T2,
 	@JvmField val t3: T3,
