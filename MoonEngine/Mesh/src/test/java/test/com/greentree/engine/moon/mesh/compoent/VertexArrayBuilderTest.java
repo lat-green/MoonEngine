e4ed71c68b6d811ee66b81b6f0929e0c7389f95c
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-11-30 05:57:18 +0300

    Add VertexArrayBuilderTest

---
+++ MoonEngine/Mesh/src/test/java/test/com/greentree/engine/moon/mesh/compoent/VertexArrayBuilderTest.java
@@ -0,0 +1,44 @@
+package test.com.greentree.engine.moon.mesh.compoent;
+
+import com.greentree.commons.math.vector.Vector3f;
+import com.greentree.engine.moon.mesh.AttributeData;
+import com.greentree.engine.moon.mesh.GraphicsMesh;
+import com.greentree.engine.moon.mesh.StaticMesh;
+import com.greentree.engine.moon.mesh.compoent.StaticMeshVertexComponent;
+import com.greentree.engine.moon.mesh.compoent.VertexArrayBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VertexArrayBuilderTest {
+
+    StaticMesh plane;
+
+    @BeforeEach
+    void init_plane() {
+        final var builder = GraphicsMesh.builder();
+        final var v1 = new Vector3f(1, 0, 0);
+        final var v2 = new Vector3f(1, 1, 0);
+        final var v3 = new Vector3f(0, 1, 0);
+        final var v4 = new Vector3f(0, 0, 0);
+        builder.addFaces(v1, v2, v3);
+        builder.addFaces(v3, v4, v1);
+        plane = builder.build();
+    }
+
+    @Test
+    void getAttributeGroup() {
+        final var builder = new VertexArrayBuilder();
+        final var group = builder.getAttributeGroup(plane, StaticMeshVertexComponent.VERTEX);
+        assertEquals(group, new AttributeData(new float[]{
+                1, 0, 0,
+                1, 1, 0,
+                0, 1, 0,
+                0, 1, 0,
+                0, 0, 0,
+                1, 0, 0
+        }, new int[]{3}));
+    }
+
+}
