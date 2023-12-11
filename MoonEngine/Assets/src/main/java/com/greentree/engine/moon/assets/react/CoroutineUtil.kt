d93586e7109a8d3bc2738a54dbfed4b87fcfeabe
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-12-11 14:29:10 +0300

    Delete React Assets

--- MoonEngine/Assets/src/main/java/com/greentree/engine/moon/assets/react/CoroutineUtil.kt
+++
@@ -1,7 +0,0 @@
-package com.greentree.engine.moon.assets.react
-
-import kotlinx.coroutines.CoroutineScope
-
-suspend fun <R : Any> currentCoroutineContext(block: CoroutineScope.() -> R): R {
-	return CoroutineScope(kotlinx.coroutines.currentCoroutineContext()).block()
-}
--- MoonEngine/Assets/src/main/java/com/greentree/engine/moon/assets/react/ReactAssetManager.kt
+++
@@ -1,9 +0,0 @@
-package com.greentree.engine.moon.assets.react
-
-import com.greentree.engine.moon.assets.react.loader.ReactAssetLoader
-import com.greentree.engine.moon.assets.serializator.manager.AssetManager
-
-interface ReactAssetManager : AssetManager {
-
-	fun addLoader(loader: ReactAssetLoader)
-}
